0e155664d736a1684d7ebf53cbdf26bfe96203a2
Maxim <maximplamadeala13@gmail.com>
2022-05-28 11:28:34 +0300

    am creeat baza de date si func. care adauga animale noi in lista

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const animalForm = document.querySelector("#animal-form")
+
+animalForm.addEventListener("submit",(e) =>{
+    e.preventDefault()
+    // console.log(e.target)
+    const animal ={
+        name:e.target.name.value,
+        imgUrl:e.target.img_url.value ,
+    description:e.target.description.value ,
+        donation:0
+    }
+    console.log(animal)
+createAnimal(animal).then(newAnimal =>{
+    console.log("succes",newAnimal)
+})
+})
+
